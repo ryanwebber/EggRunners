f319576812889d09e5eb4a13c990078e7e05848a
Ryan Webber <webber@apple.com>
2021-11-11 17:44:13 -0800

    Added swinging ground - time to work on platform 'stickiness'

---
+++ Assets/Scripts/Core/Course/Dynamic/HarmonicSwingComponent.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HarmonicSwingComponent : MonoBehaviour
+{
+    [System.Serializable]
+    private enum Axis { X, Y, Z }
+
+    [SerializeField]
+    private float initialRotationDisplacement;
+
+    [SerializeField]
+    private float armLength;
+
+    [SerializeField]
+    private float gravitationalForce;
+
+    [SerializeField]
+    private Axis rotationAxis;
+
+    [SerializeField]
+    private DynamicCourseComponent dynamicController;
+
+    private bool isRotating = false;
+    private float rotationStartTime = 0f;
+
+    private void Awake()
+    {
+        SetAngularDisplacement(initialRotationDisplacement);
+        dynamicController.OnDynamicComponentStart += () =>
+        {
+            isRotating = true;
+            rotationStartTime = Time.fixedTime;
+        };
+
+        dynamicController.OnDynamicComponentReset += () =>
+        {
+            isRotating = false;
+            SetAngularDisplacement(initialRotationDisplacement);
+        };
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isRotating)
+            return;
+
+        SetAngularDisplacement(HarmonicUtils.GetRotation(Time.fixedTime - rotationStartTime, armLength, gravitationalForce, initialRotationDisplacement));
+    }
+
+    private void SetAngularDisplacement(float theta)
+    {
+        switch (rotationAxis)
+        {
+            case Axis.X:
+                transform.localRotation = Quaternion.Euler(theta, 0f, 0f);
+                break;
+            case Axis.Y:
+                transform.localRotation = Quaternion.Euler(0f, theta, 0f);
+                break;
+            case Axis.Z:
+                transform.localRotation = Quaternion.Euler(0f, 0f, theta);
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Geometry/HarmonicUtils.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public static class HarmonicUtils
+{
+    public static float GetRotation(float time, float length, float gravity, float initialAngularDisplacement)
+    {
+        float w = MathF.Sqrt(gravity / length);
+        return initialAngularDisplacement * MathF.Cos(w * time);
+    }
+}
